fd5d68cce215196580d5bf88c12c7894cf7fd2f2
danieleBattistelli <bomber81@gmail.com>
2025-03-21 13:59:07 +0100

    feat: footer complete

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <!-- ...existing code... -->
+</head>
+<body>
+    <!-- ...existing code... -->
+    <footer style="background-image: url('{{ asset('img/footer-bg.jpg') }}');">
+        <!-- ...existing code... -->
+    </footer>
+    <!-- ...existing code... -->
+</body>
+</html>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,68 @@
-<footer>
-    <hr>
-    <h3>footer del mio sito</h3>
+<footer class="bg-light text-center text-lg-start" style="background-image: url('{{ asset('img/footer-bg.jpg') }}'); background-size: cover; background-repeat: no-repeat;">
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0" id="footer">
+                <div class="row">
+                    <div class="col">
+                        <h5 class="text-uppercase text-white"><strong>DC COMICS</strong> </h5>
+                        <ul class="list-unstyled">
+                            <li><a href="{{ route('collectibles') }}" class="text-white" style="text-decoration: none;">Collectibles</a></li>
+                            <li><a href="{{ route('comics') }}" class="text-white" style="text-decoration: none;">Comics</a></li>
+                            <li><a href="{{ route('characters') }}" class="text-white" style="text-decoration: none;">Characters</a></li>
+                            <li><a href="{{ route('movies') }}" class="text-white" style="text-decoration: none;">Movies</a></li>
+                            <li><a href="{{ route('tv') }}" class="text-white" style="text-decoration: none;">TV</a></li>
+                            <li><a href="{{ route('games') }}" class="text-white" style="text-decoration: none;">Games</a></li>
+                            <li><a href="{{ route('videos') }}" class="text-white" style="text-decoration: none;">Videos</a></li>
+                            <li><a href="{{ route('news') }}" class="text-white" style="text-decoration: none;">News</a></li>
+                        </ul>
+                        <h5 class="text-uppercase text-white"><strong>SHOP</strong> </h5>
+                        <ul class="list-unstyled">
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">Shop DC </a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h5 class="text-uppercase text-white"> <strong>DC</strong></h5>
+                        <ul class="list-unstyled">
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">Terms of Use</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">Privacy policy (New)</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">Ad Choices</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">Advertising</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">Jobs</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">Subscriptions</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">Talent Workshops</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">CPSC Certificates</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">Ratings</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">Shop Help</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h5 class="text-uppercase text-white"><strong>SITES</strong> </h5>
+                        <ul class="list-unstyled">
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">DC</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">MAD Magazine</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">DC Kids</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">DC Universe</a></li>
+                            <li><a href="#!" class="text-white" style="text-decoration: none;">DC Power Visa</a></li>
+                        </ul>
+                        <img src="{{ asset('img/dc-logo-bg.png') }}" alt="DC Logo" style="position: absolute; right: 10em; bottom: -80em; width: 30%;">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="text-center p-3" style="background-color: #303030; position: relative; min-height: 65px;">
+        <button class="btn btn-outline-primary text-white" style="border-color: blue; background-color: transparent; position: absolute; left: 10vw;">
+            SIGN-UP NOW!
+        </button>
+        <div style="display: inline-block; position: absolute; right: 10vw;">
+            <span class="text-primary" style="vertical-align: middle;">FOLLOW US</span>
+            <a href="#"><img src="{{ asset('img/footer-facebook.png') }}" alt="Facebook" width="30" height="30" style="margin-left: 10px; vertical-align: middle;"></a>
+            <a href="#"><img src="{{ asset('img/footer-twitter.png') }}" alt="Twitter" width="30" height="30" style="margin-left: 10px; vertical-align: middle;"></a>
+            <a href="#"><img src="{{ asset('img/footer-youtube.png') }}" alt="YouTube" width="30" height="30" style="margin-left: 10px; vertical-align: middle;"></a>
+            <a href="#"><img src="{{ asset('img/footer-periscope.png') }}" alt="Periscope" width="30" height="30" style="margin-left: 10px; vertical-align: middle;"></a>
+            <a href="#"><img src="{{ asset('img/footer-pinterest.png') }}" alt="Pinterest" width="30" height="30" style="margin-left: 10px; vertical-align: middle;"></a>
+        </div>
+    </div>
 </footer>
